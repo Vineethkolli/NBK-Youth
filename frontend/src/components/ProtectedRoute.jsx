031cf4d28ecea8661694268bb68a727225b264a2
Vineethkolli <kz8413@srmist.edu.in>
2024-12-04 20:09:04 +0530

    Loading Page Updated

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div>HI</div>;
   }
 
   if (!user) {
