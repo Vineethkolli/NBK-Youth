20ac0ae45f0bf9f5c0fad84242d712a6ada17077
Vineethkolli <kz8413@srmist.edu.in>
2024-12-05 12:41:00 +0530

    signin updated

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -50,6 +50,9 @@ function ProtectedRoute({ children }) {
           <span className="text-gray-800">APP starts in:</span>{' '}
           <span className="text-green-600">{timeLeft} seconds</span>
         </p>
+        <p className="text-md text-gray-600 text-center mb-6">
+          Thank you for your patience...
+        </p>
       </div>
     );
   }
