c061ef3ef3ea91e818b3739a021964884a8c4d45
Vineethkolli <kz8413@srmist.edu.in>
2024-12-12 18:45:32 +0530

    notifications updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
 );
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/sw.js')
+    .register('/sw.js', { scope: '/' })
     .then(registration => {
       console.log('Service Worker registered with scope:', registration.scope);
     })
