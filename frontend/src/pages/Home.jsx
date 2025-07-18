61095bc86924e80ba8a951892b7290d39b876601
Vineeth Kolli <kollivineeth15@gmail.com>
2025-07-18 11:09:48 +0530

    UI Changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -35,7 +35,7 @@ function Home() {
           <div className="flex justify-end">
             <button
               onClick={() => setIsEditing(!isEditing)}
-              className="inline-flex items-center px-4 py-2  rounded-md"
+              className="inline-flex items-center px-2 rounded-md"
             >
               <Edit2 className="h-4 w-4 mr-2" />
               {isEditing ? 'Done' : 'Edit'}
