7c770be1380505d7f761e4a8bcafdf68cc1c4ac7
Vineeth Kolli <kollivineeth15@gmail.com>
2025-10-19 19:15:16 +0530

    removed uptie robot, mongodb alive

--- backend/server.js
+++ backend/server.js
@@ -115,8 +115,8 @@ mongoose.connect(process.env.MONGODB_URI)
     process.exit(1); 
   });
 
-// Notification Scheduler runs at 7:00, 7:05, 7:15, 7:35, 7:55 AM IST IST every day
-cron.schedule('0,5,15,35,55 7 * * *', async () => {
+// Notification Scheduler runs at 7:00, 7:15, 7:35, 7:55 AM IST IST every day
+cron.schedule('0,15,35,55 7 * * *', async () => {
   try {
     console.log('Running scheduled notifications');
     await processDueNotifications();
