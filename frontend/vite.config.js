937bb8e2906f56dcf92a9da557eae58ee24a90be
Vineeth Kolli <kollivineeth15@gmail.com>
2025-10-22 10:36:36 +0530

    removed cache updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -24,7 +24,6 @@ export default defineConfig({
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,svg,gif,woff,woff2}'],
         runtimeCaching: [
           {
             urlPattern: ({ url }) => url.origin === location.origin,
@@ -32,8 +31,7 @@ export default defineConfig({
             options: { cacheName: 'assets-cache' }
           }
         ]
-      },
-      includeAssets: ['offline.html', 'logo/192.png']
+      }
     })
   ]
 });
