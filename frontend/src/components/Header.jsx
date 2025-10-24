08eb2207314b79847b1279dc14ae6ced16c1e46c
Vineeth Kolli <kollivineeth15@gmail.com>
2025-10-24 15:25:52 +0530

    UI, added mobile nav buttons

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Menu, User } from 'lucide-react';
+import { Menu, User, LayoutGrid, Bell } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
 function Header({ toggleSidebar }) {
@@ -14,28 +14,48 @@ function Header({ toggleSidebar }) {
   return (
     <header className="bg-white shadow-sm h-14 fixed top-0 left-0 right-0 z-20">
       <div className="h-full px-4 flex items-center justify-between">
-        {/* Menu Button */}
-        <button
-          onClick={handleMenuClick}
-          className="p-2 hover:bg-gray-100 rounded-lg"
-          aria-label="Toggle Sidebar"
-        >
-          <Menu className="h-6 w-6" />
-        </button>
+        <div className="flex items-center space-x-2">
+          <button
+            onClick={handleMenuClick}
+            className="p-2 hover:bg-gray-100 rounded-lg"
+            aria-label="Toggle Sidebar"
+          >
+            <Menu className="h-6 w-6" />
+          </button>
 
-        {/* App Title */}
+          {/* Explore visible only in large screens */}
+          <button
+            onClick={() => navigate('/explore')}
+            className="hidden md:flex p-2 hover:bg-gray-100 rounded-lg"
+            aria-label="Explore"
+          >
+            <LayoutGrid className="h-6 w-6" />
+          </button>
+        </div>
+
         <div className="flex items-center space-x-2">
           <h1 className="text-2xl font-bold text-yellow-700">NBK Youth</h1>
         </div>
 
-        {/* Profile Button */}
-        <button
-          onClick={() => navigate('/profile')}
-          className="p-2 hover:bg-gray-100 rounded-full"
-          aria-label="Profile"
-        >
-          <User className="h-6 w-6" />
-        </button>
+         <div className="flex items-center space-x-2">
+          {/* Notifications Button */}
+          <button
+            onClick={() => navigate('/notifications')}
+            className="p-2 hover:bg-gray-100 rounded-full"
+            aria-label="Notifications"
+          >
+            <Bell className="h-6 w-6" />
+          </button>
+
+          {/* Profile Button */}
+          <button
+            onClick={() => navigate('/profile')}
+            className="p-2 hover:bg-gray-100 rounded-full"
+            aria-label="Profile"
+          >
+            <User className="h-6 w-6" />
+          </button>
+        </div>
       </div>
     </header>
   );
