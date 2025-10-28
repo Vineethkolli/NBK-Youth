cf1fcef0b31ec3ca44a3ff330de2060dfcfc7adc
Vineeth Kolli <kollivineeth15@gmail.com>
2025-10-28 09:39:28 +0530

    updated packages

--- backend/routes/developer.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { auth, checkRole } from '../middleware/auth.js';
-import { developerController } from '../controllers/developerController.js';
-
-const router = express.Router();
-
-router.delete('/clear/:type',  auth, checkRole(['developer']), developerController.clearData);
-
-export default router;
