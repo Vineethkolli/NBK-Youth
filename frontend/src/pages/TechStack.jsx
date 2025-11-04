9deb9a33d391ad90bfac985d3ef5e51b59039d4c
Vineeth Kolli <kollivineeth15@gmail.com>
2025-11-04 12:31:32 +0530

    records preview downloading updated

--- frontend/src/pages/TechStack.jsx
+++ frontend/src/pages/TechStack.jsx
@@ -3,14 +3,18 @@ import { ExternalLink, Share2 } from "lucide-react";
 import { SiYoutube, SiInstagram } from "react-icons/si";
 import TechStackDetails from "../components/techstack/Technologies";
 import MindMap from "../components/techstack/MindMap";
-import Access from '../components/techstack/Access';
-import { toast } from 'react-hot-toast';
+import Access from "../components/techstack/Access";
+import { toast } from "react-hot-toast";
 
 function TechStack() {
   const [websites] = useState([
     {
       name: "Current Website",
-      url: "https://nbkyouth.vercel.app",
+      url: "https://nbkyouth.vercel.app/",
+    },
+    {
+      name: "Previous Website",
+      url: "https://sites.google.com/view/nbkyouthgangavaram",
     },
   ]);
 
@@ -32,78 +36,84 @@ function TechStack() {
     },
   ];
 
-// Share function 
-const handleShare = (url) => {
-  if (navigator.share) {
-    navigator.share({
-      title: "NBK Youth",
-      text: "NBK Youth Gangavaram",
-      url,
-    }).catch(() => {
-      toast.error("Failed to share. Please try again.");
-    });
-  } else {
-    toast.error("Share not supported on this browser.");
-  }
-};
-
+  // Share function
+  const handleShare = (url) => {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "NBK Youth",
+          text: "NBK Youth Gangavaram",
+          url,
+        })
+        .catch(() => {
+          toast.error("Failed to share. Please try again.");
+        });
+    } else {
+      toast.error("Share not supported on this browser.");
+    }
+  };
 
   return (
-<div className="space-y-6">
-  <div className="flex flex-col md:flex-row items-center justify-between mb-16">
-    {/* Social Media Section */}
-    <div className="mb-4">
-      <span className="block font-semibold text-gray-400">Follow us</span>
-      <div className="flex items-center space-x-4 mt-2">
-        <SiYoutube
-          className="cursor-pointer h-6 w-6 text-red-500"
-          title="YouTube"
-          onClick={() =>
-            window.open("https://www.youtube.com/@sivakoniki7335", "_blank", "noopener,noreferrer")
-          }
-        />
-        <SiInstagram
-          className="cursor-pointer h-6 w-6 text-pink-500"
-          title="Instagram"
-          onClick={() =>
-            window.open("https://www.instagram.com/mana_station_gangavaram/?igsh=MXU5cjM1ajVpemJm#", "_blank", "noopener,noreferrer")
-          }
-        />
-      </div>
-    </div>
+    <div className="space-y-6">
+      <div className="flex flex-col md:flex-row items-center justify-between mb-16">
+        {/* Social Media Section */}
+        <div className="mb-4">
+          <span className="block font-semibold text-gray-500">Follow us</span>
+          <div className="flex items-center space-x-4 mt-2">
+            <SiYoutube
+              className="cursor-pointer h-6 w-6 text-red-500"
+              title="YouTube"
+              onClick={() =>
+                window.open(
+                  "https://www.youtube.com/@sivakoniki7335",
+                  "_blank",
+                  "noopener,noreferrer"
+                )
+              }
+            />
+            <SiInstagram
+              className="cursor-pointer h-6 w-6 text-pink-500"
+              title="Instagram"
+              onClick={() =>
+                window.open(
+                  "https://www.instagram.com/mana_station_gangavaram/?igsh=MXU5cjM1ajVpemJm#",
+                  "_blank",
+                  "noopener,noreferrer"
+                )
+              }
+            />
+          </div>
+        </div>
 
-    {/* Website Links Section */}
-<div className="space-y-4">
-  {websites.map((site) => (
-    <div
-      key={site.name}
-      className="flex items-center justify-between text-gray-700"
-    >
-      <div className="flex items-center space-x-3">
-        <span>{site.name}</span>
-        <ExternalLink
-          className="h-4 w-4 cursor-pointer hover:text-indigo-600"
-          title="Open Link"
-          onClick={() =>
-            window.open(site.url, "_blank", "noopener,noreferrer")
-          }
-        />
-        <Share2
-          className="h-4 w-4 cursor-pointer hover:text-indigo-600"
-          title="Share Link"
-          onClick={() => handleShare(site.url)}
-        />
-      </div>
-    </div>
-  ))}
-</div>
+        {/* Website Links Section */}
+        <div className="space-y-4">
+          {websites.map((site) => (
+            <div
+              key={site.name}
+              className="flex items-center justify-between text-gray-700"
+            >
+              <div className="flex items-center space-x-3">
+                <span>{site.name}</span>
+                <ExternalLink
+                  className="h-4 w-4 cursor-pointer hover:text-indigo-600"
+                  title="Open Link"
+                  onClick={() =>
+                    window.open(site.url, "_blank", "noopener,noreferrer")
+                  }
+                />
+                <Share2
+                  className="h-4 w-4 cursor-pointer hover:text-indigo-600"
+                  title="Share Link"
+                  onClick={() => handleShare(site.url)}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
 
-    <div className="space-y-4"><br></br>  
-  </div>  
-
-        {/* Logo with Developer Name */}
-        <div className="md:w-1/2 text-center  ">
-          <div className="relative inline-block ">
+        {/* Developer Section */}
+        <div className="md:w-1/2 text-center mt-6 md:mt-0">
+          <div className="relative inline-block">
             <img
               src="developerImage.png"
               alt="Developer"
@@ -119,7 +129,9 @@ const handleShare = (url) => {
 
       {/* Principles Section */}
       <section className="mb-16">
-        <h2 className="text-2xl font-semibold text-gray-900 mb-8">Principles</h2>
+        <h2 className="text-2xl font-semibold text-gray-900 mb-8">
+          Principles
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {mottos.map((motto) => (
             <div
@@ -135,13 +147,11 @@ const handleShare = (url) => {
       </section>
 
       <MindMap />
-
       <Access />
-
       <TechStackDetails />
-      
     </div>
   );
 }
 
 export default TechStack;
+  
