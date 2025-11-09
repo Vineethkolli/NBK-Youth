d785c5d49b10db51c0a7b99a3c3e84cba6ba54e6
Vineeth Kolli <kollivineeth15@gmail.com>
2025-11-09 19:05:10 +0530

    App Updatees dilog

--- frontend/src/components/common/AppUpdate.jsx
+++ frontend/src/components/common/AppUpdate.jsx
@@ -63,7 +63,7 @@ function UpdateDialog() {
             onClick={handleReload}
             className="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition"
           >
-            Reload & Update
+            Reload
           </button>
         </div>
       </div>
