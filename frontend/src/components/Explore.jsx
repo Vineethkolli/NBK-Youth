8c6391d9f3453a8a6a2fd3614010e8ee1b36b9ac
Vineeth Kolli <kollivineeth15@gmail.com>
2025-11-12 15:52:58 +0530

    UI

--- frontend/src/components/Explore.jsx
+++ frontend/src/components/Explore.jsx
@@ -1,34 +1,7 @@
 import { Link } from "react-router-dom";
-import {
-  Home,
-  User,
-  History,
-  FolderOpen,
-  UserCog,
-  Users,
-  Bell,
-  ShieldCheck,
-  Settings,
-  IndianRupee,
-  DollarSign,
-  Trash2,
-  CheckSquare,
-  BarChart2,
-  Terminal,
-  MusicIcon,
-  CameraIcon,
-  TrophyIcon,
-  Calculator,
-  Layers,
-  LayoutDashboard,
-  FileClock,
-  Cpu,
-  Globe2,
-  CreditCard,
-  LockKeyhole,
-  Code2,
-  Fingerprint,
-} from "lucide-react";
+import { Home, User, History, FolderOpen, UserCog, Users, Bell, ShieldCheck, Settings, IndianRupee, DollarSign,
+  Trash2, CheckSquare, BarChart2, Terminal, MusicIcon, CameraIcon, TrophyIcon, Calculator, Layers, LayoutDashboard,
+  FileClock, Cpu, Globe2, CreditCard, LockKeyhole, Code2, Fingerprint } from "lucide-react";
 import { useAuth } from "../context/AuthContext";
 import Footer from "./Footer";
 
