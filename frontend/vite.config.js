702c6343b2d32aa92dd9a204d6b81649a5504469
Vineeth Kolli <kollivineeth15@gmail.com>
2026-02-05 11:20:23 +0530

    Removed offline fallback, restored

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -21,13 +21,12 @@ export default defineConfig({
       registerType: 'autoUpdate',
 
       injectManifest: {
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
       },
 
       includeAssets: [
-        '/logo/**/*.png',
+        '/logo/*.png',
         'developerImage.png',
-        'index.html',
       ],
       
       manifest: {
